868c133f53d4c8f1b9c55ad2231de94bb3794cac
Julia Glinka <julia.w.glinka@gmail.com>
2023-10-15 19:40:03 +0200

    Add definition of consts thisApp and getElem

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,43 @@
 {
-    'use strict'
+  'use strict';
 
-    thisApp = this;
+  const thisApp = this;
 
-    thisApp.data = dataSource;
-    thisApp.getElem = {};
+  thisApp.data = dataSource;
+  const getElem = {};
 
-        console.log('thisApp',thisApp);
+  console.log('thisApp',thisApp);
 
-    //referances
-    getElem.dom = {};
+  //referances
+  getElem.dom = {};
 
-    getElem.dom.booksList = document.querySelector('.books-list');
-    getElem.dom.template = document.querySelector('#template-book');
+  getElem.dom.booksList = document.querySelector('.books-list');
+  getElem.dom.template = document.querySelector('#template-book');
 
-        console.log('booksList:',getElem.dom.booksList);
-        console.log('DOM template',getElem.dom.template);
+  console.log('booksList:',getElem.dom.booksList);
+  console.log('DOM template',getElem.dom.template);
 
-    const booksSource = dataSource.books;
-        console.log('booksSource',booksSource);
+  const booksSource = dataSource.books;
+  console.log('booksSource',booksSource);
 
-    const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-        console.log('template', template);
+  const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+  console.log('template', template);
 
-    const render = function(array) {
-        for(let bookData of array){ //to tablica
-                console.log('bookData',bookData);
+  const render = function(booksArray) {
+    for(let bookData of booksArray){ 
+      console.log('bookData',bookData);
 
-            const  generatedHTML = template(bookData);
-                console.log('generated HTML',generatedHTML); //szablon z podstawionymi danymi ksiazki jako string
+      const  generatedHTML = template(bookData);
+      console.log('generated HTML',generatedHTML); //szablon z podstawionymi danymi ksiazki jako string
             
-            const generatedDOM = utils.createDOMFromHTML(generatedHTML); 
-                console.log('generatedDOM',generatedDOM); //string szablonu z danymi zamieniony na element DOM
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML); 
+      console.log('generatedDOM',generatedDOM); //string szablonu z danymi zamieniony na element DOM
 
-            getElem.dom.booksList.appendChild(generatedDOM); //wstawienie elementu DOM do rodzica w HTML
-        }
+      getElem.dom.booksList.appendChild(generatedDOM); //wstawienie elementu DOM do rodzica w HTML
     }
+  };
 
-    render(booksSource);
+  render(booksSource);
     
     
 }
