debb25bb7b22b6d72c0df3cf4ae81422ff0146f1
Julia Glinka <julia.w.glinka@gmail.com>
2023-10-16 22:23:35 +0200

    Comment consol logs

--- src/js/script.js
+++ src/js/script.js
@@ -14,24 +14,24 @@
   getElem.dom.booksList = document.querySelector('.books-list');
   getElem.dom.template = document.querySelector('#template-book');
 
-  console.log('booksList:',getElem.dom.booksList);
-  console.log('DOM template',getElem.dom.template);
+  //console.log('booksList:',getElem.dom.booksList);
+  //console.log('DOM template',getElem.dom.template);
 
   const booksSource = dataSource.books;
-  console.log('booksSource',booksSource);
+  //console.log('booksSource',booksSource);
 
   const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
-  console.log('template', template);
+  //console.log('template', template);
 
   const render = function(booksArray) {
     for(let bookData of booksArray){ 
-      console.log('bookData',bookData);
+      //console.log('bookData',bookData);
 
       const  generatedHTML = template(bookData);
-      console.log('generated HTML',generatedHTML); //szablon z podstawionymi danymi ksiazki jako string
+      //console.log('generated HTML',generatedHTML); //szablon z podstawionymi danymi ksiazki jako string
             
       const generatedDOM = utils.createDOMFromHTML(generatedHTML); 
-      console.log('generatedDOM',generatedDOM); //string szablonu z danymi zamieniony na element DOM
+      //console.log('generatedDOM',generatedDOM); //string szablonu z danymi zamieniony na element DOM
 
       getElem.dom.booksList.appendChild(generatedDOM); //wstawienie elementu DOM do rodzica w HTML
     }
@@ -51,12 +51,12 @@
 
       //reference to clicked element link DOM as before as elem
       const clickedElement = event.target.offsetParent;
-      console.log('clickedElement',clickedElement);
+      //console.log('clickedElement',clickedElement);
       const  elem = clickedElement;
         
       //get attribute data-id from clicked elem
       const dataId = elem.getAttribute('data-id');
-      console.log('data-id:',dataId);
+      //console.log('data-id:',dataId);
 
       //check if clicked element contain class 'book__image' as before in elem of booksImages
       if(elem.classList.contains('book__image')){ 
@@ -64,7 +64,7 @@
       
         //get attribute class from elem and make const about it
         const classElem = elem.getAttribute('class');
-        console.log('class of elem',classElem);
+        //console.log('class of elem',classElem);
 
         //check if class of elem is not equal to 'book__image favorite'
         if(classElem!='book__image favorite'){
@@ -86,13 +86,13 @@
           favoriteBooks.splice(indexOfDataId,1);
         
         }
-        console.log('favoriteBooks',favoriteBooks);
+        //console.log('favoriteBooks',favoriteBooks);
       }
 
     });
   };
 
-
   initActions();
+  
 
 }
